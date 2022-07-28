b28c57fd7e0b6f7c311ec165f22629439851961e
vasvap <vasvap@gmail.com>
2022-07-28 23:16:22 +0300

    added allure steps and attachments

--- tests/test_student_registration_form.py
+++ tests/test_student_registration_form.py
@@ -1,4 +1,4 @@
-
+import allure
 from selene import have, command
 from selene.support.shared import browser
 from selene.support.shared.jquery_style import s
@@ -8,93 +8,79 @@ from qa_guru_python_10_jenkins.controls.datepicker import Datepicker
 from qa_guru_python_10_jenkins.controls.dropdown import Dropdown
 from qa_guru_python_10_jenkins.controls.table import Table
 from qa_guru_python_10_jenkins.controls.tags_input import TagsInput
-from qa_guru_python_10_jenkins.utils import resource, arrange_student_registration_form_opened
-
-
-class Subjects:
-    maths = 'Maths'
-    english = 'English'
-    physics = 'Physics'
-
-
-class Hobbies:
-    sports = 'Sports'
-    reading = 'Reading'
-    music = 'Music'
-
-
-class Student:
-    first_name = 'Vasily'
-    last_name = 'Apolonov'
-    email = 'test@mail.com'
-    mobile = '9101112233'
-    gender = 'Male'
-    year_of_birth = '1974'
-    month_of_birth = 8
-    day_of_birth = '09'
-    date_of_birth = '09 September,1974'
-    address = 'Russia, Nizhny Novgorod'
-    state = 'Uttar Pradesh'
-    city = 'Lucknow'
+from utils import attach
+from utils.helpers import Student, Subjects, Hobbies
+from utils.utils import resource, arrange_student_registration_form_opened
 
 
 def test_submit_automation_practice_form():
-    arrange_student_registration_form_opened()
+    with allure.step('Preconditions and open page for test'):
+        arrange_student_registration_form_opened()
 
-    browser.open('/automation-practice-form')
-    browser.should(have.title('ToolsQA'))
-    s('.main-header').should(have.exact_text('Practice Form'))
+    with allure.step('Check page title'):
+        browser.should(have.title('ToolsQA'))
 
-    # Filling the form
-    s('#firstName').type(Student.first_name)
-    s('#lastName').type(Student.last_name)
-    s('#userEmail').type(Student.email)
+    with allure.step('Check page header'):
+        s('.main-header').should(have.exact_text('Practice Form'))
 
-    mobile_number = s('#userNumber')
-    mobile_number.type(Student.mobile)
+    with allure.step('Enter student name'):
+        s('#firstName').type(Student.first_name)
+    with allure.step('Enter student last name'):
+        s('#lastName').type(Student.last_name)
+    with allure.step('Enter student email'):
+        s('#userEmail').type(Student.email)
+    with allure.step('Enter student mobile number'):
+        mobile_number = s('#userNumber')
+        mobile_number.type(Student.mobile)
 
-    # Select Gender (radio-button)
-    gender_group = s('#genterWrapper')
-    gender_group.all('.custom-radio').element_by(have.exact_text(Student.gender)).click()
+    with allure.step('Select student gender'):
+        gender_group = s('#genterWrapper')
+        gender_group.all('.custom-radio').element_by(have.exact_text(Student.gender)).click()
 
-    # Select Date of Birth
-    date_of_birth = Datepicker(s('#dateOfBirthInput'))
-    date_of_birth.set_date_of_birth(year=Student.year_of_birth,
-                                    month=Student.month_of_birth,
-                                    day=Student.day_of_birth)
+    with allure.step('Select student date of birth'):
+        date_of_birth = Datepicker(s('#dateOfBirthInput'))
+        date_of_birth.set_date_of_birth(year=Student.year_of_birth,
+                                        month=Student.month_of_birth,
+                                        day=Student.day_of_birth)
+    with allure.step('Set Subjects'):
+        subject = TagsInput(s('#subjectsInput'))
+        subject.add(Subjects.maths)
+        # subject.add(Subjects.maths).add(Subjects.english).add(Subjects.physics)
+        subject.autocomplete(from_='Eng', to_=Subjects.english)
+        subject.add_or_auto('Phys')
 
-    # Set Subjects
-    subject = TagsInput(s('#subjectsInput'))
-    subject.add(Subjects.maths)
-    # subject.add(Subjects.maths).add(Subjects.english).add(Subjects.physics)
-    subject.autocomplete(from_='Eng', to_=Subjects.english)
-    subject.add_or_auto('Phys')
+    with allure.step('Set hobbies'):
+        set_hobbies.set_hobby(Hobbies.sports)
+        set_hobbies.set_hobby(Hobbies.music)
 
-    # Set hobbies - checkbox
-    set_hobbies.set_hobby(Hobbies.sports)
-    set_hobbies.set_hobby(Hobbies.music)
+    with allure.step('Load a file'):
+        s('#uploadPicture').send_keys(resource('picture.png'))
 
-    # Load a file
-    s('#uploadPicture').send_keys(resource('picture.png'))
+    with allure.step('Enter the address'):
+        s('#currentAddress').type(Student.address)
 
-    # Filling the address
-    s('#currentAddress').type(Student.address)
+    with allure.step('Select state and city'):
+        set_state_city = Dropdown(s('#state'), s('#city'))
+        set_state_city.select(state_data=Student.state)
+        set_state_city.autocomplete(city_data=Student.city)
 
-    set_state_city = Dropdown(s('#state'), s('#city'))
-    set_state_city.select(state_data=Student.state)
-    set_state_city.autocomplete(city_data=Student.city)
+    with allure.step('Send the form'):
+        s('#submit').perform(command.js.click)
 
-    s('#submit').perform(command.js.click)
+    with allure.step('Check student data'):
+        results = Table(s('.modal-content .table'))
+        results.cell(1, 1).should(have.text(f'{Student.first_name} {Student.last_name}'))
+        results.cell(2, 1).should(have.text(Student.email))
+        results.cell(3, 1).should(have.text(Student.gender))
+        results.cell(4, 1).should(have.text(Student.mobile))
+        results.cell(5, 1).should(have.text(Student.date_of_birth))
+        results.cell(6, 1).should(have.text(f'{Subjects.maths}, {Subjects.english}, {Subjects.physics}'))
+        results.cell(7, 1).should(have.text(f'{Hobbies.sports}, {Hobbies.music}'))
+        results.cell(8, 1).should(have.text('picture.png'))
+        results.cell(9, 1).should(have.text(Student.address))
+        results.cell(10, 1).should(have.text(f'{Student.state} {Student.city}'))
 
-    # Assert
-    results = Table(s('.modal-content .table'))
-    results.cell(1, 1).should(have.text(f'{Student.first_name} {Student.last_name}'))
-    results.cell(2, 1).should(have.text(Student.email))
-    results.cell(3, 1).should(have.text(Student.gender))
-    results.cell(4, 1).should(have.text(Student.mobile))
-    results.cell(5, 1).should(have.text(Student.date_of_birth))
-    results.cell(6, 1).should(have.text(f'{Subjects.maths}, {Subjects.english}, {Subjects.physics}'))
-    results.cell(7, 1).should(have.text(f'{Hobbies.sports}, {Hobbies.music}'))
-    results.cell(8, 1).should(have.text('picture.png'))
-    results.cell(9, 1).should(have.text(Student.address))
-    results.cell(10, 1).should(have.text(f'{Student.state} {Student.city}'))
+    attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    # attach.add_video(browser)
---
+++ utils/helpers.py
@@ -0,0 +1,25 @@
+class Subjects:
+    maths = 'Maths'
+    english = 'English'
+    physics = 'Physics'
+
+
+class Hobbies:
+    sports = 'Sports'
+    reading = 'Reading'
+    music = 'Music'
+
+
+class Student:
+    first_name = 'Vasily'
+    last_name = 'Apolonov'
+    email = 'test@mail.com'
+    mobile = '9101112233'
+    gender = 'Male'
+    year_of_birth = '1974'
+    month_of_birth = 8
+    day_of_birth = '09'
+    date_of_birth = '09 September,1974'
+    address = 'Russia, Nizhny Novgorod'
+    state = 'Uttar Pradesh'
+    city = 'Lucknow'
